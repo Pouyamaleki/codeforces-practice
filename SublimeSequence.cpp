70f26a619b3c2668ef9c8348d021db747c5559f6
Pouya <pouyamaleki85@gmail.com>
2025-11-04 21:04:30 +0330

    SublimeSequence

---
+++ SublimeSequence.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 0, n = 0;
+    cin >> x;
+    cin >> n;
+    if (n % 2 == 0)
+        cout << "0" << endl;
+    else
+        cout << x << endl;
+    return 0;
+}
